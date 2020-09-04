0db781d8decfe34ee03f4a348862a984cad4354f
JNchengge <chenghao5678@stu2019.jnu.edu.cn>
2020-09-04 22:24:07 +0800

    update

--- test.py
+++ test.py
@@ -1,27 +1,2 @@
-class X():
-    def f(self):
-        print('x')
-
-class A(X):
-    def f(self):
-        print('a')
-    def extra(self):
-        print("extra a")
-
-class B(X):
-    def f(self):
-        print('b')
-    def extra(self):
-        print("extra b")
-
-class C(A,B,X):           
-    def f(self):
-        super(C,self).f()  #super函数将C类转换成了父类A
-        print("c")
-
-print("调用顺序")
-print(C.mro())             #使用mro函数可知，C类的调用方式为广度优先搜索
-
-c=C()
-c.f()
-c.extra()
+#!/usr/bin/python3
+print("wdnmd")
