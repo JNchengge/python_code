e3fe1fe9677b6fdc86fd82c102f7cfe2feaef73f
JNchengge <chenghao5678@stu2019.jnu.edu.cn>
2020-09-05 13:36:32 +0800

    update

--- 3.5.14.py
+++ 3.5.14.py
@@ -1,25 +1,15 @@
 import random
 a=random.randint(0,100)
 b=random.randint(0,100)
-m=a
-n=b
-if m>n:
-    r=m-int(m/n)*n
-    while r:
-        m=n
-        n=r
-        r=m-int(m/n)*n
+m,n=max(a,b),min(a,b)
+r=m-int(m/n)*n
+while r:
     m=n
-    common_diviser=m
-elif m<n:
-    r=n-int(n/m)*m
-    while r:
-        n=m
-        m=r
-        r=n-int(n/m)*m
-    n=m
-    common_diviser=n
-elif m==n:
+    n=r
+    r=m-int(m/n)*n
+m=n
+common_diviser=m
+if m==n:
     common_diviser=m
 common_multiple=(a*b)/common_diviser
 print("整数1：%d，整数2：%d"%(a,b))
