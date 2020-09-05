3851edefb2554de98e9dd2c7b0511b4062d8a197
JNchengge <chenghao5678@stu2019.jnu.edu.cn>
2020-09-05 13:37:19 +0800

    update

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
-#!/usr/bin/python3
-print("wdnmd")
+import copy
+#!/usr/bin/
+deeper=[4,5,6]
+origin=[1,2,3,deeper]
+_copy=origin.copy()
+z=origin
+x=[1,2,3,deeper]
+print(id(deeper))
+print(id(origin))
+print(id(_copy))
+print(deeper is [3])
+print(z is origin)
+print(x is origin)
+print(x[3] is deeper)
+_copy=copy.deepcopy(origin)
