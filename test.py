62620f6e38986e26f862c94b36ffc443148bf76b
JNchengge <chenghao5678@stu2019.jnu.edu.cn>
2020-09-05 14:26:15 +0800

    update

--- test.py
+++ test.py
@@ -1,15 +1,3 @@
-import copy
-#!/usr/bin/
-deeper=[4,5,6]
-origin=[1,2,3,deeper]
-_copy=origin.copy()
-z=origin
-x=[1,2,3,deeper]
-print(id(deeper))
-print(id(origin))
-print(id(_copy))
-print(deeper is [3])
-print(z is origin)
-print(x is origin)
-print(x[3] is deeper)
-_copy=copy.deepcopy(origin)
+a=[1,2,3]
+a.clear()
+print(a)
