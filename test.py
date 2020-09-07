44a233cd06a0bb2fce030b47ab045d86aabed796
JNchengge <chenghao5678@stu2019.jnu.edu.cn>
2020-09-07 18:36:09 +0800

    chapter9

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-def greater(x,y):
-    return x>y
-print(list(map(greater,[1,2,3,5,6],[2,3,4,1,2,3])))
+list1=[1,2,3]
+list2=[3,4,5]
+dict1={'1':list1,'2':list2}
+dict2=dict1.copy()
+dict1['1'][0]=15
+print(dict1['1'][0]+dict2['1'][0])  
